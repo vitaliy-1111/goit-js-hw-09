079418fad4365c58259d04795d6adae965e228a9
Vitaliy <networkvetalius@gmail.com>
2021-12-20 15:09:32 +0200

    01-color-switcher done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
 
+const refs = {
+  btnStartColorPicker: document.querySelector('button[data-start]'),
+  btnStopColorPicker: document.querySelector('button[data-stop]')
+}
+let colorPickerId = null;
+
+refs.btnStartColorPicker.addEventListener('click', onBtnStartColorPicker);
+refs.btnStopColorPicker.addEventListener('click', onBtnStopColorPicker);
+
+refs.btnStopColorPicker.setAttribute("disabled", "disabled");
+
+function onBtnStartColorPicker() {
+  colorPickerId = setInterval(() => { document.body.style.backgroundColor = getRandomHexColor();}, 1000);
+  refs.btnStartColorPicker.setAttribute("disabled", "disabled");
+  refs.btnStopColorPicker.removeAttribute("disabled", "disabled");
+}
+
+function onBtnStopColorPicker() {
+  clearInterval(colorPickerId);
+  refs.btnStartColorPicker.removeAttribute("disabled", "disabled");
+  refs.btnStopColorPicker.setAttribute("disabled", "disabled");
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
